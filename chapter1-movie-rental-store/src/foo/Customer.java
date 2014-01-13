483bf04ac62e12ff2a480954c5f30f98dd59dfb6
magic <jksun@thoughtworks.com>
2014-01-13 22:36:15 +0800

    [jk] extract mothod from Customer statement

--- chapter1-movie-rental-store/src/foo/Customer.java
+++ chapter1-movie-rental-store/src/foo/Customer.java
@@ -24,47 +24,73 @@ public class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
-        Enumeration rentals = _rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
 
+        Enumeration rentals = _rentals.elements();
         while (rentals.hasMoreElements()) {
-            double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
 
-            //determine amounts for each line
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    break;
-            }
-
-            // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-                frequentRenterPoints++;
-
-            // show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(getRenterAmount(each)) + "\n";
         }
 
-        // add footer lines
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        result += "Amount owed is " + String.valueOf(getTotalRenterAmount()) + "\n";
+        result += "You earned " + String.valueOf(getTotalRenterPoints()) + " frequent renter points";
 
         return result;
+    }
+
+    private double getTotalRenterAmount() {
+        Enumeration rentals = _rentals.elements();
+        double totalAmount = 0;
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+
+            totalAmount += getRenterAmount(each);
+        }
+
+        return totalAmount;
+    }
+
+    private int getTotalRenterPoints() {
+        Enumeration rentals = _rentals.elements();
+        int frequentRenterPoints = 0;
+
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+
+
+            frequentRenterPoints += getFrequentPoint(each);
+        }
+
+        return frequentRenterPoints;
+    }
+
+    private int getFrequentPoint(Rental each) {
+        if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) {
+            return 2;
+        }
+        else {
+            return 1;
+        }
+    }
+
+    private double getRenterAmount(Rental each) {
+        double thisAmount = 0;
+        switch (each.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (each.getDaysRented() > 2)
+                    thisAmount += (each.getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += each.getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (each.getDaysRented() > 3)
+                    thisAmount += (each.getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return thisAmount;
     }
 }
